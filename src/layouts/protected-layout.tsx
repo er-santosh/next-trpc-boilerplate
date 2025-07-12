56fa9b78a94a0797f954bdde9ff4fdbdb916e646
Santosh Tharu <er.santoshtharu@gmail.com>
2025-07-12 08:36:32 +0545

    chore: make dashboard height changes

--- src/layouts/protected-layout.tsx
+++ src/layouts/protected-layout.tsx
@@ -12,11 +12,13 @@ const style = {
 const ProtectedLayout = ({ children }: PropsWithChildren) => (
   <SidebarProvider style={style as React.CSSProperties}>
     <AppSidebar variant="inset" />
-    <SidebarInset>
+    <SidebarInset className="flex flex-col h-[calc(100vh-16px)] overflow-hidden">
       <AppHeader />
-      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">{children}</div>
+      <div className="flex-1 overflow-y-auto">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 lg:px-6">
+          <div className="flex flex-1 flex-col">
+            <div className="@container/main flex flex-1 flex-col gap-2">{children}</div>
+          </div>
         </div>
       </div>
     </SidebarInset>
